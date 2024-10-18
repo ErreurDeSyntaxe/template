a35fe58362cec24fb5b1a3c68707cfa2840e078c
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-10-18 14:15:53 +0800

    Add JS doc comments example

--- src/js/model.js
+++ src/js/model.js
@@ -3,8 +3,13 @@ class Person {
     this.firstName = firstName;
     this.lastName = lastName;
   }
-  greet() {
-    return `Hello. My name is ${this.firstName} ${this.lastName}.`;
+  /**
+   * Introduces the Person with their greeting of choice and full name
+   * @param {string} [greeting='Hello']
+   * @returns {string} An introduction with a greeting a full name
+   */
+  greet(greeting = 'Hello') {
+    return `${greeting}. My name is ${this.firstName} ${this.lastName}.`;
   }
 }
 
