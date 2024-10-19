7b4befe5e78b9099e739f5ce2aabbc69244ac59d
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-10-19 15:31:36 +0800

    Import as "model" from model.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,6 @@
-import * as People from './model.js'; // import the inner workings
+import * as model from './model.js'; // import the inner workings
 import view from './view.js'; // import the outer workings
 
 // use both inner & outer workings
-view.renderMessage(People.jane.greet());
-view.renderMessage(People.john.greet());
+view.renderMessage(model.jane.greet());
+view.renderMessage(model.john.greet());
