6587d21483bce554103f82b74de35063a6e6f8e1
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2025-03-20 21:31:32 +0800

    Remove Webpack (happily)

--- src/js/img.js
+++
@@ -1,7 +0,0 @@
-// How to add images thru JS using webpack
-import previewImg from '../../README/project-preview.jpg';
-
-const image = document.createElement('img');
-image.src = previewImg;
-
-document.body.appendChild(image);
